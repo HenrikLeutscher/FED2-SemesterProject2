9f9e6d1f19c40d0bdb5b232ab8b34fbd791268a6
HenrikLeutscher <HenrikHezo@hotmail.com>
2025-12-13 17:16:28 +0100

    API config and storage utilities

---
+++ js/utils/storage.js
@@ -0,0 +1,30 @@
+import { API_KEY } from "../config.js";
+
+export function saveToken(token) {
+  localStorage.setItem("accessToken", token);
+}
+
+export function getToken() {
+    return localStorage.getItem("accessToken");
+}
+
+export function saveUser(user) {
+  localStorage.setItem("user", JSON.stringify(user));
+}
+
+export function getUser() {
+  const user = localStorage.getItem("user");
+  return user ? JSON.parse(user) : null;
+}
+
+export function removeToken() {
+  localStorage.removeItem("accessToken");
+}
+
+export function removeUser() {
+  localStorage.removeItem("user");
+}
+
+export function getApiKey() {
+    return API_KEY;
+}
