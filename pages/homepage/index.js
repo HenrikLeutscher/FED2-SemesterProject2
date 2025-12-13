5da02bb89586f6e280d173a3d12428175dcf3622
HenrikLeutscher <HenrikHezo@hotmail.com>
2025-12-13 17:20:12 +0100

    Home page features

---
+++ pages/homepage/index.js
@@ -0,0 +1,9 @@
+import { renderEndingSoon } from "/js/listings/endingsoon.js";
+import { renderNewestListings } from "/js/listings/newestListings.js";
+import { listingStats } from "/js/listings/listingstats.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderEndingSoon();
+    renderNewestListings();
+    listingStats();
+});
